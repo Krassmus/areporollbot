8083e646b5715027d812aaa99a0497acf4e68070
Rasmus Fuhse <krassmus@gmail.com>
2020-03-26 13:52:24 +0100

    add help and limit

--- areporollbot.php
+++ areporollbot.php
@@ -21,24 +21,37 @@ function roll($dice) {
     return $output;
 }
 
+$message = null;
 
 $body = json_decode(file_get_contents("php://input"), true);
 if (!isset($body['message']['text'])) {
     $body = $_REQUEST;
 }
-preg_match("/^\/roll\s+(\d+)/", $body['message']['text'], $matches);
-$dice = $matches[1];
-if ($dice) {
-    $result = roll($dice);
-
+if (stripos($body['message']['text'], "/help") === 0) {
+    $message = "This is a bot for the roleplaying game Arepo. And these are my commands you can use:\n\n";
+    $message .= "*/help* : Get this info.\n\n";
+    $message .= "*/roll 4* : Roll 4 six-sided dice, each 1 erases itself and the highest other die, and after that only the three highest dice get added together. This is a result between 0 and 18.";
+}
+if (stripos($body['message']['text'], "/roll") === 0) {
+    preg_match("/^\/roll\s+(\d+)/", $body['message']['text'], $matches);
+    $dice = $matches[1];
+    if ($dice) {
+        $result = roll($dice);
+        if ($dice <= 1000) {
+            $message = $body['message']['from']['first_name'] . " rolled: \n" . implode(" + ", $result['rolled']) . " => *" . $result['result'] . "*";
+        } else {
+            $message = $body['message']['from']['first_name'] . " rolled: *" . $result['result'] . "*";
+        }
+    }
+}
+if ($message !== null) {
     $message = array(
         'method' => "sendMessage",
         'chat_id' => $body['message']['chat']['id'],
-        'text' => $body['message']['from']['first_name'] . " rolled: \n" . implode(" + ", $result['rolled']) . " => *" . $result['result'] . "*",
+        'text' => $message,
         'parse_mode' => "MarkdownV2"
     );
     $message = json_encode($message);
-
     header("Status-Code: 200");
     header("Version: HTTP/1.1");
     header("Content-Type: application/json");
