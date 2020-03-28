54ca266deff2a5c2cc85a10e7c871925e842fe1a
Rasmus Fuhse <krassmus@gmail.com>
2020-03-28 23:17:03 +0100

    work on

---
+++ dist.config.php
@@ -0,0 +1,10 @@
+<?php
+
+//You need to copy this file to config.php (without the dist) and
+//set username, password and database-name and the api-key of your bot.
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+$pdo = new PDO("mysql:dbname=arepo;host=localhost;charset=utf8", "username", "password");
+$apikey = "myapi:key";
